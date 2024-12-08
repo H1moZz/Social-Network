e5a4c6153c4d315d414551d320ca7af3e64f5464
Даниил <shabanovdan11l@mail.ru>
2024-12-08 20:40:23 +0300

    Добавление моделей

--- SocialNetwork/__init__.py
+++ SocialNetwork/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///socialnetwork.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app) 
+
 from SocialNetwork import routes
+from SocialNetwork import models
