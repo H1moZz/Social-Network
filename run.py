9df2b4548cd36be863db37f20f8d5d6073d85f26
Даниил <shabanovdan11l@mail.ru>
2025-01-31 13:43:30 +0300

    Попытка деплоя

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 from social_network.app import create_app
 from social_network.routes.web_socket import socketio
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 myapp = create_app()
 socketio.init_app(myapp, cors_allowed_origins="*")
