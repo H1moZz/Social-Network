5372bd0be77a77a21ab16d8e732ee3615e81bf5a
Даниил <shabanovdan11l@mail.ru>
2025-05-27 21:15:16 +0300

    fix

--- social_network/config.py
+++ social_network/config.py
@@ -1,21 +1,14 @@
 import os
-from flask_jwt_extended import JWTManager
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'socialnetwork.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        'poolclass': 'QueuePool',
-        'pool_size': 10,
-        'max_overflow': 20,
-        'pool_timeout': 30,
-        'pool_recycle': 1800,
-    }
     UPLOAD_FOLDER = BASE_DIR + '/static/pf_photos'
     BASE_URL = os.getenv('REACT_APP_API_BASE_URL', 'http://localhost:3001')
     TESTING = False
 
 class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR,'test_socialnetwork.db')}"
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR,'test_socialnetwork.db')}"
     TESTING = True
--- social_network/run.py
+++
@@ -1,3 +0,0 @@
-if __name__ == '__main__':
-    # Запуск приложения с SocketIO
-    socketio.run(myapp, host="0.0.0.0", port=3001, allow_unsafe_werkzeug=True) 
